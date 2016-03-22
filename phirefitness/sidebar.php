c4d205b808167db46cd9ddf4987e70bc393e5a06
MayankS94 <mayank1021@hotmail.com>
2016-03-22 17:22:41 -0400

    Update sidebar.php

--- phirefitness/sidebar.php
+++ phirefitness/sidebar.php
@@ -1,13 +1,33 @@
-<?php
+<?php 
+
+/*
+* Theme Name: PhireFitness Theme
+* Theme URI: https://phoenix.sheridanc.on.ca/~ccit3424/
+* Description: Theme File
+* Authors/Contributors: Mayank Sharma, Nayab Safdar and Shruti Shirke (MSN Developers)
+* Version: 1.0 03/22/2016
+* Author URI: https://phoenix.sheridanc.on.ca/~ccit3424/
+*/ 
+
+if(!is_page()){?>
+	<div id="sidebar" class="widget-area side-widget-area" role="complementary">
+	<?php if(is_active_sidebar( 'the_sidebar' ) ):?>
+	<div class="sidebar-widgets">
+	<?php dynamic_sidebar( 'the_sidebar' );?>
+	</div>
+	<?php endif;?>
+	</div>
+<?php }
 /**
- * The sidebar containing the main widget area.
+ * The sidebar containing the main widget area. The conditional statement states that if the sidebar is active, print it, if 
+ not, do not make it appear on the site. "Secondary" and "widget=area" define the section where the sidebar is located. "widget-area" is another word for "sidebar" in this case. 
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package phirefitness
  */
-
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+ 
+ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
