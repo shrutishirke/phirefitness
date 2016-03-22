c4e38cb2b07968f0054d3330fb95711f91fede34
shrutishirke <shruti.shirke27@gmail.com>
2016-03-22 17:14:32 -0400

    Update content-page.php

--- phirefitness/template-parts/content-page.php
+++ phirefitness/template-parts/content-page.php
@@ -4,6 +4,10 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
+ The following code displays the content in page.php. The arry is assigned to page-links so that ehy can show up within content and be clickable on the front page - which is entry content. The footer will also have all the names of the pages which will
+ be clickable. If there are no pages, the array will not print them to be displayed on the front end. Also, if these pages are not 
+ published in the WP dashboard, they will not be displayed. The code edit_post_link also gives users the chance to edit the 
+ anmes of the pages. 
  * @package phirefitness
  */
 
