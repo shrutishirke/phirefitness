b935aa17b9caa15bda84517a407ad885429bcedf
MayankS94 <mayank1021@hotmail.com>
2016-03-22 17:24:50 -0400

    Update single.php

--- phirefitness/single.php
+++ phirefitness/single.php
@@ -5,6 +5,7 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package phirefitness
+ The single.php page is sort of like an archives page. The single.php file is used insted of index.php when the page only contains one post, otherwise index.php will be used to display all posts.The loop says the same below, if there is one post, get it and load up the body, header etc anyway, if there is no post dont load any posts, if there is more than one post, dont load it from here - the loop in index.php will take care of it and if there is at least one comment, load that template.  
  */
 
 get_header(); ?>
